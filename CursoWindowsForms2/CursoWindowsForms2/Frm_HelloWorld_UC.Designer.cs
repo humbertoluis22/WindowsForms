5f7d5eb9671053a5b9cb75199cf6612950d92f5b
humbertoluis22 <humbertoluis2013@gmail.com>
2023-11-06 12:59:24 -0300

    atualizando tabpages

---
+++ CursoWindowsForms2/CursoWindowsForms2/Frm_HelloWorld_UC.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CursoWindowsForms2
+{
+    partial class Frm_HelloWorld_UC
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txt_ConteudoLabel = new System.Windows.Forms.TextBox();
+            this.Btn_MoficaLabel = new System.Windows.Forms.Button();
+            this.Lbl_Titulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Txt_ConteudoLabel
+            // 
+            this.Txt_ConteudoLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Txt_ConteudoLabel.Location = new System.Drawing.Point(20, 101);
+            this.Txt_ConteudoLabel.Name = "Txt_ConteudoLabel";
+            this.Txt_ConteudoLabel.Size = new System.Drawing.Size(501, 20);
+            this.Txt_ConteudoLabel.TabIndex = 6;
+            // 
+            // Btn_MoficaLabel
+            // 
+            this.Btn_MoficaLabel.Location = new System.Drawing.Point(20, 49);
+            this.Btn_MoficaLabel.Name = "Btn_MoficaLabel";
+            this.Btn_MoficaLabel.Size = new System.Drawing.Size(234, 33);
+            this.Btn_MoficaLabel.TabIndex = 5;
+            this.Btn_MoficaLabel.Text = "Modifica Texto da Label";
+            this.Btn_MoficaLabel.UseVisualStyleBackColor = true;
+            this.Btn_MoficaLabel.Click += new System.EventHandler(this.Btn_MoficaLabel_Click);
+            // 
+            // Lbl_Titulo
+            // 
+            this.Lbl_Titulo.AutoSize = true;
+            this.Lbl_Titulo.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Titulo.Location = new System.Drawing.Point(16, 23);
+            this.Lbl_Titulo.Name = "Lbl_Titulo";
+            this.Lbl_Titulo.Size = new System.Drawing.Size(238, 23);
+            this.Lbl_Titulo.TabIndex = 4;
+            this.Lbl_Titulo.Text = "Visual Studio .Net Version";
+            // 
+            // Frm_HelloWorld_UC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Txt_ConteudoLabel);
+            this.Controls.Add(this.Btn_MoficaLabel);
+            this.Controls.Add(this.Lbl_Titulo);
+            this.Name = "Frm_HelloWorld_UC";
+            this.Size = new System.Drawing.Size(537, 330);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt_ConteudoLabel;
+        private System.Windows.Forms.Button Btn_MoficaLabel;
+        private System.Windows.Forms.Label Lbl_Titulo;
+    }
+}
